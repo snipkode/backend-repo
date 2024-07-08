88af2ce3213613e8066b95a918be20ffc8d5c542
Alam Wibowo <alamhafidz@gmail.com>
2024-07-08 10:17:28 +0700

    Testing Development Hot Replacement Build after changes with concurrently

--- core/app.ts
+++ core/app.ts
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
