0ceabdf0be146be056cd4cc7074cde0082be333c
Alam Wibowo <alamhafidz@gmail.com>
2024-07-08 09:49:50 +0700

    Add Service Account Firebase Config

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,11 @@
+import admin from 'firebase-admin';
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+export { db };
